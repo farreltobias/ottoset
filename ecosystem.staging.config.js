11922329eb447f3793ca1eda3a7078cc074aebb0
Davi Lhlapak Rosa <davilhlapak@hotmail.com>
2022-10-25 14:19:28 -0300

    ci: fixed nvm version in ecosystem

--- ecosystem.staging.config.js
+++ ecosystem.staging.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: `ottoset-front-web-3098`,
-      script: `yarn next start -p 3098`,
+      script: `source ~/.nvm/nvm.sh && nvm use 16 && yarn next start -p 3098`,
     },
   ],
 };
