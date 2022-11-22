8cb2a9dd8c3ae056f0c024946fd2ca9c4b78fb56
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2022-11-22 16:49:55 -0300

    feat(provider): :sparkles: added form integration

--- src/pages/prestador.tsx
+++ src/pages/prestador.tsx
@@ -1,5 +1,9 @@
+import { useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+
+import { z } from 'zod';
 
 import { Button } from '@components/Button';
 import { Input } from '@components/Form/Input';
@@ -10,12 +14,95 @@ import { Overlaid } from '@components/Texts/Overlaid';
 
 import { SEO } from '@seo/prestador';
 
-import { SubmitHandler } from '@unform/core';
+import { Body } from './api/provider';
+
+import { FormHandles, SubmitHandler } from '@unform/core';
 import { Form } from '@unform/web';
 
 const Prestador: NextPage = () => {
-  const handleSubmit: SubmitHandler<any> = (data) => {
-    console.log(data);
+  const formRef = useRef<FormHandles>(null);
+  const [sending, setSending] = useState(false);
+
+  const telephoneRegex = /^\(\d{2}\) (9?\d{4})-\d{4}$/;
+
+  const required = z.string().min(1, 'Campo obrigatório');
+
+  const schema = z.object({
+    name: required,
+    email: required.email('E-mail inválido'),
+    telephone: required.regex(telephoneRegex, 'Telefone inválido'),
+    state: required,
+    city: required,
+    area: required,
+  });
+
+  const defaultToasterOptions = {
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  };
+
+  const handleSubmit: SubmitHandler<Body> = async (data) => {
+    if (sending) return;
+
+    setSending(true);
+    const id = toast.loading('Enviando contato...');
+
+    try {
+      formRef.current?.setErrors({});
+
+      await schema.parseAsync(data);
+
+      const response = await fetch('/api/provider', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      const json = await response.json();
+
+      setSending(false);
+
+      toast.update(id, {
+        ...defaultToasterOptions,
+        render: 'Contato enviado com sucesso!',
+        type: 'success',
+        isLoading: false,
+      });
+
+      return json;
+    } catch (error) {
+      setSending(false);
+
+      if (error instanceof z.ZodError) {
+        toast.update(id, {
+          ...defaultToasterOptions,
+          render: 'Por favor, verifique os campos',
+          type: 'error',
+          isLoading: false,
+        });
+
+        const errors = (error as z.ZodError<z.infer<typeof schema>>).formErrors;
+
+        console.log(errors);
+
+        Object.entries(errors.fieldErrors).forEach(([field, [message]]) => {
+          formRef.current?.setFieldError(field, message);
+        });
+      } else {
+        toast.update(id, {
+          ...defaultToasterOptions,
+          render: 'Ocorreu um erro ao enviar o contato',
+          type: 'error',
+          isLoading: false,
+        });
+      }
+    }
   };
 
   const radioOptions = [
@@ -33,7 +120,7 @@ const Prestador: NextPage = () => {
 
   const formInputs = [
     {
-      name: 'nome',
+      name: 'name',
       label: 'Nome completo',
       placeholder: 'Digite seu nome',
     },
@@ -43,7 +130,7 @@ const Prestador: NextPage = () => {
       placeholder: 'Digite seu email',
     },
     {
-      name: 'phone',
+      name: 'telephone',
       label: 'Celular (Whatsapp)',
       placeholder: 'Digite seu telefone',
       mask: '(99) 99999-9999',
@@ -77,6 +164,7 @@ const Prestador: NextPage = () => {
         </Overlaid>
 
         <Form
+          ref={formRef}
           onSubmit={handleSubmit}
           initialData={{ area: '1' }}
           className="lg:child:w-2/3 flex flex-col"
