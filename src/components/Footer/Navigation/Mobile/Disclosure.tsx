8764102f6507de1910b7c2fc6bd84a537540f23f
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2022-12-19 08:52:50 -0300

    feat(footer): :sparkles: added framer-motion

--- src/components/Footer/Navigation/Mobile/Disclosure.tsx
+++ src/components/Footer/Navigation/Mobile/Disclosure.tsx
@@ -1,44 +1,50 @@
 import { Fragment, ReactNode } from 'react';
 
+import { AnimatePresence, motion } from 'framer-motion';
+
 import Arrow from '@public/navigation/arrow-right.svg';
 
 import { Title } from '@components/Texts';
 
-import { Disclosure as Menu, Transition } from '@headlessui/react';
+import { Disclosure as Menu } from '@headlessui/react';
 
 type Props = {
   label: string;
   children: ReactNode[];
-  id: string;
 };
 
-export const Disclosure: React.FC<Props> = ({ label, children, id }) => {
+export const Disclosure: React.FC<Props> = ({ label, children }) => {
   return (
     <Menu as="li" className="inline-block relative w-full">
-      <Menu.Button className="text-left w-full">
-        <Title
-          as="span"
-          variant="h6"
-          className="flex items-center justify-between"
-        >
-          {label}
-          <Arrow className="transition-transform duration-300 my-3 transform ui-open:-rotate-90" />
-        </Title>
-      </Menu.Button>
+      {({ open }) => (
+        <>
+          <Menu.Button className="text-left w-full">
+            <Title
+              as="span"
+              variant="h6"
+              className="flex items-center justify-between"
+            >
+              {label}
+              <Arrow className="transition-transform duration-300 my-3 transform ui-open:-rotate-90" />
+            </Title>
+          </Menu.Button>
 
-      <Transition
-        as={Fragment}
-        enter="transition-all ease-disclosure duration-300"
-        enterFrom="transform h-0"
-        enterTo={`transform footer-disclosure-item-height-${id}`}
-        leave="transition-all ease-disclosure duration-300"
-        leaveFrom={`transform footer-disclosure-item-height-${id}`}
-        leaveTo="transform h-0"
-      >
-        <Menu.Panel as="ul" className="w-full overflow-hidden">
-          {children}
-        </Menu.Panel>
-      </Transition>
+          <AnimatePresence>
+            {open && (
+              <Menu.Panel
+                static
+                as={motion.ul}
+                className="w-full overflow-hidden"
+                initial={{ height: 0 }}
+                animate={{ height: 'auto' }}
+                exit={{ height: 0 }}
+              >
+                {children}
+              </Menu.Panel>
+            )}
+          </AnimatePresence>
+        </>
+      )}
     </Menu>
   );
 };
--- src/components/Footer/Navigation/Mobile/index.tsx
+++ src/components/Footer/Navigation/Mobile/index.tsx
@@ -1,74 +1,29 @@
-import { Children, useEffect, useState } from 'react';
-import Head from 'next/head';
-
-import { useWindowSize } from '@hooks/useWindowSize';
+import { Children } from 'react';
 
 import { navLinks } from '@data/navLinks';
 
 import { Disclosure } from './Disclosure';
 import { SubItem } from './SubItem';
 
-const getClassNames = (width: number) => {
-  const stylesString = navLinks
-    .filter(({ onlyHeader }) => !onlyHeader)
-    .reduce((acc, { subItems }, index) => {
-      if (!subItems) return acc;
-
-      const className = `footer-disclosure-item-height-${index}`;
-      const PADDING = 32 * 2;
-      const size = width - PADDING;
-
-      const height = subItems.reduce((acc, { label }) => {
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-
-        if (!context) return acc;
-
-        context.font = '500 0.875rem Inter, sans-serif';
-        const { width: textWidth } = context.measureText(label);
-
-        return acc + 1.75 + (Math.ceil(textWidth / size) - 1) * 1.25;
-      }, 0);
-
-      const style = `.${className} { height: ${height}rem; }\n`;
-      return acc + style;
-    }, '');
-
-  return <style>{stylesString}</style>;
-};
-
 export const Mobile: React.FC<React.PropsWithChildren> = () => {
-  const { width } = useWindowSize();
-
-  const [styles, setStyles] = useState<JSX.Element>();
-
-  useEffect(() => {
-    if (!width) return;
-
-    setStyles(getClassNames(width));
-  }, [width]);
-
-  const NavList = navLinks
-    .filter(({ onlyHeader }) => !onlyHeader)
-    .map(
-      ({ label, subItems }, index) =>
-        subItems && (
-          <Disclosure label={label} id={index.toString()}>
-            {Children.toArray(
-              subItems.map(({ href, label, target }) => (
-                <SubItem href={href} label={label} target={target} />
-              )),
-            )}
-          </Disclosure>
-        ),
-    );
+  const navList = navLinks.filter(({ onlyHeader }) => !onlyHeader);
 
   return (
-    <>
-      <Head>{styles}</Head>
-      <ul className="flex justify-center items-center lg:hidden flex-col h-full w-full mb-6">
-        {Children.toArray(NavList)}
-      </ul>
-    </>
+    <ul className="flex justify-center items-center lg:hidden flex-col h-full w-full mb-6">
+      {Children.toArray(
+        navList.map(
+          ({ label, subItems }) =>
+            subItems && (
+              <Disclosure label={label}>
+                {Children.toArray(
+                  subItems.map(({ href, label, target }) => (
+                    <SubItem href={href} label={label} target={target} />
+                  )),
+                )}
+              </Disclosure>
+            ),
+        ),
+      )}
+    </ul>
   );
 };
